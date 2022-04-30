c5eb30f9b8eaec1d242ab907d5020c03400d3123
Sergey Zlobin <szlobin@lincolnlabs.co>
2022-04-30 14:49:02 +0500

    New employee form fields like controlled components. Update this component to class component.

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,25 +1,52 @@
+import { Component } from 'react';
+
 import './employees-add-form.css';
 
-const EmployeesAddForm = () => {
-  return (
-    <div className="app-add-form">
-      <h3>Добавьте нового сотрудника</h3>
+class EmployeesAddForm extends Component {
+  constructor(props) {
+    super(props);
 
-      <form className="add-form d-flex">
-        <input
-          type="text"
-          className="form-control new-post-label"
-          placeholder="Как его зовут?" />
-        <input
-          type="number"
-          className="form-control new-post-label"
-          placeholder="З/П в $?" />
-        <button
-          type="submit"
-          className="btn btn-outline-light">Добавить</button>
-      </form>
-    </div>
-  );
+    this.state = {
+      name: '',
+      salary: '',
+    }
+  }
+
+  onValueChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    })
+  }
+
+  render() {
+    const { name, salary } = this.state;
+
+    return (
+      <div className="app-add-form">
+        <h3>Добавьте нового сотрудника</h3>
+
+        <form className="add-form d-flex">
+          <input className="form-control new-post-label"
+            type="text"
+            name="name"
+            value={name}
+            placeholder="Как его зовут?"
+            onChange={this.onValueChange} />
+
+          <input className="form-control new-post-label"
+            type="number"
+            name="salary"
+            value={salary}
+            placeholder="З/П в $?"
+            onChange={this.onValueChange} />
+
+          <button
+            type="submit"
+            className="btn btn-outline-light">Добавить</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default EmployeesAddForm;
