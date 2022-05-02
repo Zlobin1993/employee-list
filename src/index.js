63a4e9c010100262ae26f178e93255da417cb0fd
Sergey Zlobin <szlobin@lincolnlabs.co>
2022-05-02 14:07:22 +0500

    Add global styles variables.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import App from './components/app/app';
 
-import './index.css';
+import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
