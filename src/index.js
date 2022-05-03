12f264c32364ec511ec01cca68ea12ed2686bd9b
Sergey Zlobin <szlobin@lincolnlabs.co>
2022-05-03 11:07:35 +0500

    Add react-bootstrap package.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import App from './components/app/app';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
